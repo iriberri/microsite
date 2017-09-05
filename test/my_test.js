b827f8b2e821e08a06d402449604069b10fa489f
timothytsai <timothy.tsai@noblis.org>
2017-09-05 16:11:37 -0400

    Travis Testing with Selenium

--- test/my_test.js
+++ test/my_test.js
@@ -1,10 +1,12 @@
-﻿/*
-* Carry out a Google Search
-*/
+﻿const mochaTimeout = 30000; // ms
 
-"use strict";
-var assert = require("assert");
 var webdriver = require('selenium-webdriver');
+var test = require('selenium-webdriver/testing');
+var chai = require('chai');
+var should = chai.should();
+var expect = chai.expect;
+var until = webdriver.until;
+
 var browser = new webdriver.Builder().usingServer().withCapabilities({ 'browserName': 'chrome' }).build();
 
 function logTitle() {
@@ -25,26 +27,61 @@ function findTutsPlusLink() {
 }
 
 function closeBrowser() {
-    assert.equal("success", "success", "Socrata API Call returns success");
     browser.quit();
 }
 
 browser.get('http://localhost:51611/microsite/usdot_data_microsite_template.html');
-browser.findElement(webdriver.By.id('mainSearch')).sendKeys('field test');
-browser.findElement(webdriver.By.className('searchButton')).click();
-browser.wait(findTutsPlusLink, 2000);
+//browser.findElement(webdriver.By.id('mainSearch')).sendKeys('field test');
+//browser.findElement(webdriver.By.className('searchButton')).click();
+//browser.wait(findTutsPlusLink, 2000);
 
 
-describe("First Test Suite", function () {
-    describe("Page Title Loaded", function () {
-        it("test page title load", function () {
+test.describe("Test Suite", function () {
+    this.timeout(mochaTimeout);
+    test.describe("Page Load Components", function () {
+        test.it("Title Loaded", function () {
+            browser.wait(until.elementLocated(webdriver.By.className('searchHeaderText')));
+
             browser.findElement(webdriver.By.className('searchHeaderText')).getText().then(function (text) {
-                console.log(text);
-                expect(text).to.equal("EXPLORE OUR DATA - Beta Version")
+                expect(text).to.deep.equal("EXPLORE OUR DATA - Beta Version");
+            });
+        });
+        test.it("Contact Email Loaded", function () {
+            browser.wait(until.elementLocated(webdriver.By.id('contactEmail')));
+
+            browser.findElement(webdriver.By.id('contactEmail')).getText().then(function (text) {
+                expect(text).to.deep.equal("data.itsjpo@dot.gov");
+            });
+        });
+        test.it("Categories Loaded", function () {
+            browser.wait(until.elementLocated(webdriver.By.id('bterm0')));
+
+            browser.findElement(webdriver.By.id('bterm0')).then(function (text) {
+                expect(text).to.exist;
+            });
+        });
+        test.it("Datasets Loaded", function () {
+            browser.wait(until.elementLocated(webdriver.By.id('fds0')));
+
+            browser.findElement(webdriver.By.id('fds0')).then(function (text) {
+                expect(text).to.exist;
             });
         });
     });
+
+    test.describe("Search Related Components", function () {
+        test.it("Search Submitted", function () {
+            browser.findElement(webdriver.By.id('mainSearch')).sendKeys('research results');
+            browser.findElement(webdriver.By.className('searchButton')).click();
+
+            browser.findElement(webdriver.By.id('resultsQuery')).getText().then(function (text) {
+                expect(text).to.equal("research results");
+            });
+        });
+        test.it("NTL Results Available", function () {
+            browser.findElement(webdriver.By.className('resultsCount')).getText().then(function (text) {
+                expect(parseInt(text)).to.be.above(23);
+            });
+        })
+    });
 });
-
-
-closeBrowser();
